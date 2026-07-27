03bc7ed8897c9eaff60e537f43bbf30319c090cc
Vansh Deep Singh <vs493599@gmail.com>
2026-07-27 14:57:30 +0530

    New Additions

--- server/src/index.js
+++ server/src/index.js
@@ -44,8 +44,8 @@ app.use('/api/client-upload', authMiddleware, clientUploadRoutes);
 
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB Connected!'))
+//   .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(5000, () => console.log('Express server started on http://localhost:5000'));
